8aba91f587005461fe8ac3be80b9e51a568323f1
Dom van Almsick <dominicvana@gmail.com>
2022-06-05 04:45:23 -0500

    feat: useEffect fetching books

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Library from './components/Library';
 import Search from './components/Search';
+import * as BooksAPI from './utils/booksAPI';
 
 function App() {
   const [books, setBooks] = useState([]);
 
+  useEffect(() => {
+    BooksAPI.getAll().then(setBooks).catch(console.error);
+  }, []);
+
   const changeShelf = (book, newShelf) => {
     const otherBooks = books.filter(b => b.id !== book.id);
     const updatedBook = { ...book, shelf: newShelf };
-    const newState = [...otherBooks, updatedBook];
 
-    setBooks(newState);
+    setBooks([...otherBooks, updatedBook]);
   };
 
   return (
