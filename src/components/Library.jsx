9ea6e04e3ef4d26b75754c6508bc02320107f7be
Dom van Almsick <dominicvana@gmail.com>
2022-06-05 07:01:43 -0500

    refactor: BookShelf to own file

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,59 +1,6 @@
 import { Link } from 'react-router-dom';
 import Header from './Header';
-
-const SHELVES = [
-  { value: 'currentlyReading', label: 'Currently Reading' },
-  { value: 'wantToRead', label: 'Want to Read' },
-  { value: 'read', label: 'Read' },
-];
-
-const BookShelf = ({ books, shelf, onShelfChange }) => {
-  return (
-    <div className="bookshelf">
-      <h2 className="bookshelf-title">{shelf.label}</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          {books
-            .filter(book => book.shelf === shelf.value)
-            .map(book => (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div
-                      className="book-cover"
-                      style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${book.imageLinks.thumbnail})`,
-                      }}
-                    ></div>
-                    <div className="book-shelf-changer">
-                      <select
-                        value={book.shelf}
-                        onChange={e => onShelfChange(book, e.target.value)}
-                      >
-                        <option value="move" disabled>
-                          Move to...
-                        </option>
-                        {SHELVES.map(shelf => (
-                          <option key={shelf.value} value={shelf.value}>
-                            {shelf.label}
-                          </option>
-                        ))}
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.authors}</div>
-                </div>
-              </li>
-            ))}
-        </ol>
-      </div>
-    </div>
-  );
-};
+import BookShelf, { SHELVES } from './BookShelf';
 
 const Library = ({ books, onShelfChange }) => {
   return (
