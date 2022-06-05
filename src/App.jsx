99d9ef0a0bf93c28299f4755f6d0a412a71a8e88
Dom van Almsick <dominicvana@gmail.com>
2022-06-05 04:40:16 -0500

    feat: init routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Library from './components/Library';
 import Search from './components/Search';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const changeShelf = (book, newShelf) => {
+    const otherBooks = books.filter(b => b.id !== book.id);
+    const updatedBook = { ...book, shelf: newShelf };
+    const newState = [...otherBooks, updatedBook];
+
+    setBooks(newState);
+  };
+
   return (
     <Routes>
-      <Route exact path="/" element={<Library />} />
+      <Route
+        exact
+        path="/"
+        element={<Library books={books} onShelfChange={changeShelf} />}
+      />
       <Route path="/search" element={<Search />} />
     </Routes>
   );
--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import Header from './Header';
 
-const Library = () => {
+const Library = ({ books }) => {
   return (
     <main>
       <Header />
