f1d9e5e9601e95f65c9351c57bda02a8252dbbcf
Dom van Almsick <dominicvana@gmail.com>
2022-06-06 22:22:28 -0500

    feat: add preview library

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,50 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
+import bookPlaceholder from '../assets/book-placeholder.svg';
+
+const mockData = {
+  currentlyReading: {
+    label: 'Currently reading',
+    books: [{ id: 1, title: 'Forward', author: 'Andrew Yang' }],
+  },
+  wantToRead: {
+    label: 'Want to read',
+    books: [{ id: 1, title: 'Lifespan', author: 'David Sinclair' }],
+  },
+  read: {
+    label: 'Read',
+    books: [{ id: 1, title: 'Sapiens', author: 'Yuval Noah Harari' }],
+  },
+};
+
+const PreviewShelves = () => {
+  return Object.values(mockData).map(shelf => (
+    <section key={shelf.label} className="shelf">
+      <h2>{shelf.label}</h2>
+      <BookShelf books={shelf.books} />
+    </section>
+  ));
+};
+const BookShelf = ({ books }) => {
+  return (
+    <>
+      {books.map(book => (
+        <div key={book.id} className="book-card">
+          <img
+            src={bookPlaceholder}
+            alt="placeholder book"
+            style={{ width: '48px', height: '48px', borderRadius: '8px' }}
+          />
+          <div>
+            <h3>{book.title}</h3>
+            <h4>{book.author}</h4>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 const Home = () => {
   const [library, setLibrary] = useState({
@@ -18,6 +62,8 @@ const Home = () => {
     },
   });
 
+  const [showPreview, setShowPreview] = useState(false);
+
   const totalBooks = Object.values(library).reduce(
     (total, shelf) => total + shelf.books.length,
     0
@@ -32,18 +78,28 @@ const Home = () => {
       </button>
       <main>
         {noBooks ? (
-          <section id="no-books-fallback">
-            <h2>Let's get some books for you</h2>
-            <div>
-              <h3>
-                Your library has no books (yet). Hit the search button above to
-                find your fist book.
-              </h3>
-              <button id="preview-library-btn" className="outline accent">
-                Preview
-              </button>
-            </div>
-          </section>
+          <div>
+            <section id="no-books-fallback">
+              <h2>Let's get some books for you</h2>
+              <div>
+                <h3>
+                  Your library has no books (yet). Hit the search button above
+                  to find your fist book.
+                </h3>
+                <h3>
+                  Or if you'd like to see what your libtary could look like...
+                </h3>
+                <button
+                  id="preview-library-btn"
+                  className={`outline ${showPreview ? 'accent' : ''}`}
+                  onClick={() => setShowPreview(!showPreview)}
+                >
+                  {showPreview ? 'Hide' : 'Show'} preview
+                </button>
+              </div>
+            </section>
+            <div>{showPreview ? <PreviewShelves /> : null}</div>
+          </div>
         ) : (
           <>
             <div>Your bookshelves await...</div>
@@ -55,3 +111,14 @@ const Home = () => {
 };
 
 export default Home;
+
+/*
+{showPreview
+  ? Object.values(mockData).map(shelf => (
+      <section key={shelf.label}>
+        <h2>{shelf.label}</h2>
+        <BookShelf books={shelf.books} />
+      </section>
+    ))
+  : null}
+  */
